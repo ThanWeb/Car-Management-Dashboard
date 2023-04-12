f21c6d00bfaf1ef34a3935593eb75f1dd979119c
Hans <classsickpeople@gmail.com>
2023-04-12 12:41:44 +0800

    Finish put or update feature

--- routes/notes.js
+++ routes/notes.js
@@ -2,15 +2,16 @@ var express = require('express');
 var router = express.Router();
 const Validator = require('fastest-validator')
 const v = new Validator
-const {notes} = require("../models")
+const {notes} = require("../models");
+const { HostNotFoundError } = require('sequelize');
 
-router.get("/", function(req, res, next) {
-    res.send("Hello World");
-});
+// router.get("/", function(req, res, next) {
+//     res.send("Hello World");
+// });
 
-router.get("/env", function(req, res, next) {
-    res.send(process.env.APP_NAME);
-});
+// router.get("/env", function(req, res, next) {
+//     res.send(process.env.APP_NAME);
+// });
 
 // POST
 router.post("/", async (req, res, nexy) => {
@@ -32,5 +33,29 @@ router.post("/", async (req, res, nexy) => {
     })
 })
 
+// PUT
+router.put("/:id", async (req, res, next) => {
+    const id = req.params.id
+    let note = await notes.findByPk(id)
+    if(!note) {
+        return res.status(404).json({status: 404, message: 'Data not found'})
+    }
+    // validation
+    const schema = {
+        title: "string|optional",
+        description: "string|optional"
+    }
+    const validate = v.validate(req.body, schema)
+    if(validate.length) {
+        return res.status(400).json(validate)
+    }
+    // proses update
+    note = await note.update(req.body)
+    res.json({
+        status: 200,
+        message: 'Success update data',
+        data: note
+    })
+});
 
 module.exports = router;
